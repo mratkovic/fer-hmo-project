733c664c65580256413a98fd085920b72ac24f0d
mratkovic <marko.ratkovic93@gmail.com>
2017-01-14 15:18:25 +0100

    First <4K solution

--- src/hr/fer/hmo/demo/GARunner.java
+++ src/hr/fer/hmo/demo/GARunner.java
@@ -9,6 +9,7 @@ import java.util.Random;
 import hr.fer.hmo.checker.ConstraintsChecker;
 import hr.fer.hmo.checker.FitnessCalculator;
 import hr.fer.hmo.checker.Instance;
+import hr.fer.hmo.checker.Solution;
 import hr.fer.zemris.optjava.dz4.function.IFunction;
 import hr.fer.zemris.optjava.dz4.function.InstanceFunction;
 import hr.fer.zemris.optjava.dz4.ga.GenerationGA;
@@ -19,6 +20,7 @@ import hr.fer.zemris.optjava.ga.decoder.PassThroughDecoder;
 import hr.fer.zemris.optjava.ga.mutation.IMutation;
 import hr.fer.zemris.optjava.ga.mutation.ToggleMutation;
 import hr.fer.zemris.optjava.ga.selection.ISelection;
+import hr.fer.zemris.optjava.ga.selection.RouletteWheelSelection;
 import hr.fer.zemris.optjava.ga.selection.Tournament;
 import hr.fer.zemris.optjava.ga.solution.IntArraySolution;
 
@@ -45,15 +47,17 @@ public class GARunner {
         System.out.println("Parsed...\n");
         IFunction f = new InstanceFunction(problem, calc, checker);
 
-        ISelection<IntArraySolution> selection = new Tournament<>(3);
+        ISelection<IntArraySolution> selection2 = new Tournament<>(3);
+        ISelection<IntArraySolution> selection = new RouletteWheelSelection<>();
         ICross<IntArraySolution> cross = new SinglePointCross(rnd);
-        IMutation<IntArraySolution> mutation = new ToggleMutation(rnd, 2);
+        IMutation<IntArraySolution> mutation = new ToggleMutation(rnd, 1);
         IDecoder<IntArraySolution> decoder = new PassThroughDecoder();
 
         List<IntArraySolution> population = generateInitialPopulation(rnd, problem, populationSize);
 
         GenerationGA<IntArraySolution> ga = new GenerationGA<>(f, decoder, populationSize, minError, maxIter, selection,
                 mutation, cross, rnd, 2, true, PRINT_CNT);
+
         ga.run(population);
 
     }
@@ -73,11 +77,17 @@ public class GARunner {
         for (int i = 0; i < possibleValues.length; ++i) {
             possibleValues[i] = availableServers.get(i);
         }
-
+        String solPath = "./sol_4292_92777778_1484151396.txt";
+        Solution sol = new Solution(solPath);
         List<IntArraySolution> population = new LinkedList<>();
         for (int i = 0; i < populationSize; i++) {
             IntArraySolution s = new IntArraySolution(problem.usedComponents.size(), possibleValues);
             s.randomize(rnd);
+            for (int j = 0; j < s.size(); ++j) {
+                if (rnd.nextBoolean()) {
+                    s.values[j] = sol.getCompLocationsArrray().get(j);
+                }
+            }
 
             population.add(s);
         }
